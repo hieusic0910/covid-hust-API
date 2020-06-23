7284937914ccdb4e73603a238d6b8168b766a539
hieusic0910 <hadotc622@gmail.com>
2020-06-23 21:18:44 +0700

    update api

---
+++ README.md
@@ -0,0 +1,3 @@
+API "Đồ án lập trình"
+Thời gian hoàn thành: 2 tháng"
+Trần Trung Hiếu.
--- config/index.js
+++ config/index.js
@@ -22,12 +22,7 @@ config.redis.password = process.env.REDIS_PASSWORD || '';
 
 // SCRAPER INTERVALS
 config.interval = process.env.INTERVAL || 6e5;
-// eslint-disable-next-line camelcase
-config.nyt_interval = process.env.NYT_INTERVAL || 864e5;
-// eslint-disable-next-line camelcase
-config.apple_interval = process.env.APPLE_INTERVAL || 864e5;
-// eslint-disable-next-line camelcase
-config.gov_interval = process.env.GOV_INTERVAL || 864e5;
+
 
 // SENTRY KEY (ONLY FOR PRODUCTION)
 // eslint-disable-next-line camelcase
